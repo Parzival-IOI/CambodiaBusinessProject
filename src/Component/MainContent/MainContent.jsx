bbe46e129b1e4bd18204a436325ea6de0d5b0c1c
Parzival-IOI <119472721+Parzival-IOI@users.noreply.github.com>
2022-12-17 00:32:15 +0700

    InterSection

--- src/Component/MainContent/MainContent.jsx
+++ src/Component/MainContent/MainContent.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './MainContent.css'
+import {useInView} from 'react-intersection-observer'
 
 
 const MainContent = () => {
+
+  const {ref: Element, inView: isIntersect} = useInView()
+
   return (
-    <div className='MainContent' >MainContent</div>
+    <div className='MainContent' >
+      <div className={`${isIntersect ? "open" : 'close'}`} ref={Element} >Hello There How Are You Doing</div>
+    </div>
   )
 }
 
-export default MainContent
+export default MainContent;
--- src/Component/Parallax/Parallax.jsx
+++ src/Component/Parallax/Parallax.jsx
@@ -9,6 +9,7 @@ import Fish from "../../Asset/Fish.png";
 import BackRiff from "../../Asset/BackRiff.png";
 import Turtle from "../../Asset/Turtle.png";
 import PH from "../../Asset/PH.png";
+import Mobile from '../../Asset/MobileParallax.png';
 import "./Parallax.css"
 
 import useScroll from "../../Hook/useScroll";
@@ -23,7 +24,7 @@ function Parallax() {
 
     return (
         <div className="Parallax" style={{height : `calc(120vh - (${position*0.35}px))`}}>
-            <img src={PH} alt="P." />
+            <img src={PH} alt="P." className="Hide"/>
             <LazyLoadImage
                 className="img"
                 src={Riff1}
@@ -31,46 +32,54 @@ function Parallax() {
                 style={{ "--i--n": "8", "bottom" : "0"}}
             />
             <LazyLoadImage
-                className="img"
+                className="img  Hide"
                 src={Riff2}
                 alt="P.."
                 style={{ "--i--n": "5" , 'top' : `${position*0.6}px`}}
             />
             <LazyLoadImage
-                className="img"
+                className="img Hide"
                 src={Riff3}
                 alt="P..."
                 style={{ "--i--n": "3" , 'top' : `${position*0.9}px`}}
             />
             <LazyLoadImage
-                className="img"
+                className="img  Hide"
                 src={AFish}
                 alt="P...."
                 style={{ "--i--n": "6" , 'top' : `${position*0.4 }px`, 'left' : `-${position*0.3}px`}}
             />
             <LazyLoadImage
-                className="img"
+                className="img  Hide"
                 src={BFish}
                 alt="P....."
                 style={{ "--i--n": "7" , 'top' : `${position*0.5 }px`, 'left' : `${position*0.6}px`}}
             />
             <LazyLoadImage
-                className="img"
+                className="img  Hide"
                 src={Fish}
                 alt="P......"
                 style={{ "--i--n": "4" , 'top' : `${position*0.8 }px`, 'left' : `${position*0.06}px`}}
             />
             <LazyLoadImage
-                className="img"
+                className="img  Hide"
                 src={BackRiff}
                 alt="P......."
                 style={{ "--i--n": "1" , 'top' : `${position*0.8}px`}}
             />
             <LazyLoadImage
-                className="img"
+                className="img  Hide"
                 src={Turtle}
                 alt="P........."
                 style={{ "--i--n": "2" , left : `${position*1.5}px`, top : `${position*0.6}px`}}
+            />
+
+            <LazyLoadImage 
+                className="img Hide2" 
+                src={Mobile}
+                style={{ "--i--n": "1" , 'top' : `${position*0.8}px`}}
+                placeholderSrc={PH}
+                effect='blur'
             />
             {/* <div className="Fill" style={{height : `${position*0.35}px`}}></div> */}
         </div>
