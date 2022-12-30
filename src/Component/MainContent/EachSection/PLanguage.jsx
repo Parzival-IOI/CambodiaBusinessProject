24b37c824061ee95d63a9cba8d54e70951406d54
Parzival-IOI <119472721+Parzival-IOI@users.noreply.github.com>
2022-12-30 21:28:50 +0700

    Busy-Comit

--- src/Component/MainContent/EachSection/PLanguage.jsx
+++ src/Component/MainContent/EachSection/PLanguage.jsx
@@ -6,13 +6,13 @@ const PLanguage =  React.forwardRef((props, myRef)  => {
     <section ref={myRef} className={`${props.isIntersect ? "close open" : 'close'}`}>
         <div className="text1">
             <div className="Til2">
-              <div className="animateGradientColorBG"><h4>My Knowledge</h4></div>
+              <div className="animateGradientColorBG"><h4>My Work</h4></div>
             </div>
         </div>
         <div className="perspective">
-          <img src={code} alt="Code" loading='lazy' className='Code'/>
-          <img src={code} alt="Code" loading='lazy' className='Code'/>
-          <img src={code} alt="Code" loading='lazy' className='Code'/>
+          <img src={code} alt="Code" className='Code'/>
+          <img src={code} alt="Code" className='Code'/>
+          <img src={code} alt="Code" className='Code'/>
         </div>
     </section>
   )
